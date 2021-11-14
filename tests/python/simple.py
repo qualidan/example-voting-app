8a4c977ed10e4899592a002006ebdf61e2a53a2e
Dan Amzulescu <dan.amzulescu@gmail.com>
2021-11-14 15:52:05 -0600

    ADDING SIMPLE MOCK PY TESTS

---
+++ tests/python/simple.py
@@ -0,0 +1,15 @@
+import unittest
+
+class SimplisticTest(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(True)
+
+    def test2(self):
+        self.assertTrue(True)
+
+    def test3(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
